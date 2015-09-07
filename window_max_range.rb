025c7e29b25287ffa4b133be15eef4a069dba2e3
App Academy Student <student@appacademy.io>
2015-09-06 19:05:23 -0700

    add benchmark to window_max

--- window_max_range.rb
+++ window_max_range.rb
@@ -1,5 +1,7 @@
+require "benchmark"
+
 #naive
-def windowed_max_range1(arr, w)
+def naive_windowed_max_range(arr, w)
   current_max_range = 0
   arr.each_with_index do |el,i|
     window = arr[i...i+w]
@@ -31,7 +33,31 @@ def windowed_max_range(arr, w)
 end
 
 #test
-p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
-p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
-p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
-p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+# p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6
+
+def run_benchmark(n, w)
+  arr = (0..n).to_a.shuffle
+  Benchmark.bm(7) do |x|
+    x.report("naive:"){ naive_windowed_max_range(arr, w) }
+    x.report("minmax queue:"){ windowed_max_range(arr, w) }
+  end
+end
+
+puts "small array | small window"
+run_benchmark(500, 5)
+puts "\n\n"
+
+puts "small array | large window"
+run_benchmark(500, 400)
+puts "\n\n"
+
+puts "large array | small window"
+run_benchmark(50000, 20)
+puts "\n\n"
+
+puts "large array | large window"
+run_benchmark(50000, 2000)
+puts "\n\n"
